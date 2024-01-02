7361166b0c183ee1f67a68f216eb9f346df6062c
Ajmal K A <ajmal.ka@techjays.com>
2024-01-02 21:19:21 +0530

    added

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-// this is just a test for me to do
-
 console.log("hello world")
 
-// this is test3
-// this is test4 by ajmalka
-
-// this is test 5 by jabbar updated
-
-
-// there is something more in this thing
